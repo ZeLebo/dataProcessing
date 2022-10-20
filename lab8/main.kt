c87a2aa092204b1796e46fe4891f89ab461306b3
ZeLebo <sartakov.sasha@icloud.com>
2022-10-21 01:41:27 +0700

    lab 14

--- lab8/main.kt
+++ lab8/main.kt
@@ -1,24 +1,44 @@
 package org.example;
 
 fun main(args: Array<String>) {
-    val threadsCount = if (args.size > 0) args[0].toInt() else 12
-    val iterCount = if (args.size > 1) args[1].toInt() else 1000
-    var threadIterations = (iterCount.toDouble() / threadsCount).toInt()
-    val results = Array(threadsCount) { 0.0 }
+    val threadsCount = if (args.size > 0) args[0].toInt() else Runtime.getRuntime().availableProcessors()
 
-    val threads = (0 until threadsCount).map { i ->
-        Thread {
-            var result = 0.0
-            for (j in (i * threadIterations) until ((i + 1) * threadIterations)) {
-                result += Math.pow(-1.0, j.toDouble()) / (2 * j + 1)
+    var iterCount = 0
+    var results = Array(threadsCount) { 0.0 }
+    var threads = Array(threadsCount) { Thread() }
+
+    fun startThreads() {
+        var threadIterations = (iterCount.toDouble() / threadsCount).toInt()
+        results = Array(threadsCount) { 0.0 }
+
+        threads = (0 until threadsCount).map { i ->
+            Thread {
+                var result = 0.0
+                for (j in (i * threadIterations) until ((i + 1) * threadIterations)) {
+                    result += Math.pow(-1.0, j.toDouble()) / (2 * j + 1)
+                }
+                results[i] = result
             }
-
-            results[i] = result
-        }
+        }.toTypedArray()
+        threads.forEach { it.start() }
     }
-    threads.forEach { it.start() }
-    threads.forEach { it.join() }
 
-    val pi = results.sum() * 4
-    println(pi)
+    fun joinThreads() {
+        threads.forEach { it.join() }
+        val pi = results.sum() * 4
+        println()
+        println("Iterations have been done: $iterCount")
+        println("The result is $pi")
+    }
+
+    Runtime.getRuntime().addShutdownHook(Thread {
+        joinThreads()
+    })
+
+    println("The calculation will be done when you press Ctrl+C")
+
+    while (true) {
+        iterCount +=  1000
+        startThreads()
+    }
 }
