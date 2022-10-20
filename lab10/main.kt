6a2a789e9dc66338698e9b2c1c2ebabdd86f7196
ZeLebo <sartakov.sasha@icloud.com>
2022-10-20 20:15:53 +0700

    new solution

--- lab10/main.kt
+++ lab10/main.kt
@@ -1,71 +1,67 @@
-// package org.example
-
-// fun main() {
-//     val lock = Object()
-//     var switcher = false
-//     // вообще тут должен быть mutex но он не импортировался
-//     Thread {
-//         for (i in 1..10) {
-//             synchronized(lock) {
-//                 while(!switcher) {
-//                     lock.wait()
-//                 }
-//             }
-
-//             println("Second thread $i")
-
-//             synchronized(lock) {
-//                 switcher = false
-//                 lock.notifyAll()
-//             }
-//         }
-//     }.start()
-
-//     for (i in 1..10) {
-//         synchronized(lock) {
-//             while(switcher) {
-//                 lock.wait()
-//             }
-//         }
-
-//         println("Main thread $i")
-
-//         synchronized(lock) {
-//             switcher = true
-//             lock.notifyAll()
-//         }
-//     }
-// }
-
-// print "hi" from two threads in queue using mutex
 package lab10
 
-import java.util.concurrent.Semaphore
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantLock
 
 fun main() {
-    val mutex = Semaphore(1)
-    var flag = AtomicBoolean(true)
-    // print from thread on by one
+    val mutex = ReentrantLock()
+    val asyncCondition = mutex.newCondition()
+
+    // print string in 2 threads on by one in order
     Thread {
         for (i in 1..10) {
-            while (!flag.get()) {
-                mutex.acquire()
-                flag.set(true)
-                println("Second thread $i")
-                mutex.release()
-            }
+            mutex.lock()
+            println("Second thread $i")
+            asyncCondition.signal()
+            asyncCondition.await()
+            mutex.unlock()
         }
     }.start()
 
-    Thread {
-        for (i in 1..10) {
-            while (flag.get()) {
-                mutex.acquire()
-                flag.set(false)
-                println("Main thread $i")
-                mutex.release()
-            }
-        }
-    }.start()
+    mutex.lock()
+    asyncCondition.signal()
+    asyncCondition.await()
+    mutex.unlock()
+
+    for (i in 1..10) {
+        mutex.lock()
+        println("Main thread $i")
+        asyncCondition.signal()
+        asyncCondition.await()
+        // asyncCondition.signal()
+        mutex.unlock()
+    }
+    
+    // val lock = Object()
+    // var switcher = false
+    // Thread {
+    //     for (i in 1..10) {
+    //         synchronized(lock) {
+    //             while(!switcher) {
+    //                 lock.wait()
+    //             }
+    //         }
+
+    //         println("Second thread $i")
+
+    //         synchronized(lock) {
+    //             switcher = false
+    //             lock.notifyAll()
+    //         }
+    //     }
+    // }.start()
+
+    // for (i in 1..10) {
+    //     synchronized(lock) {
+    //         while(switcher) {
+    //             lock.wait()
+    //         }
+    //     }
+
+    //     println("Main thread $i")
+
+    //     synchronized(lock) {
+    //         switcher = true
+    //         lock.notifyAll()
+    //     }
+    // }
 }
