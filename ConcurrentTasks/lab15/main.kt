6e93bee7d39bb87641981fc9c00d50d419c17712
ZhoRa <sartakov.sasha@icloud.com>
2022-11-23 23:20:30 +0700

    Working echo server

--- ConcurrentTasks/lab15/main.kt
+++ ConcurrentTasks/lab15/main.kt
@@ -1,6 +1,7 @@
 package selectserver
 
-import java.net.ServerSocket
+
+import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
@@ -8,123 +9,52 @@ import java.nio.channels.ServerSocketChannel
 import java.nio.channels.SocketChannel
 
 fun main(args: Array<String>) {
+    val port = if (args.isNotEmpty()) args[0].toInt() else 9090
+    val ip = if (args.size > 1) args[1] else "localhost"
+    val ipPort = if (args.size > 2) args[2].toInt() else 80
 
-    EchoServer().main(args)
-}
+    val selector = Selector.open()
+    val serverSocketChanel = ServerSocketChannel.open()
+    serverSocketChanel.bind(InetSocketAddress("localhost", port))
+    serverSocketChanel.configureBlocking(false)
+    serverSocketChanel.register(selector, SelectionKey.OP_ACCEPT)
 
-// using select and poll
-class EchoServer {
-    private val POISON_PILL = "POISON_PILL"
-
-    fun answerWithEcho(buffer: ByteBuffer, key: SelectionKey) {
-        val channel = key.channel() as SocketChannel
-        buffer.flip()
-        channel.write(buffer)
-        buffer.compact()
-
-        if (buffer.hasRemaining()) {
-            key.interestOps(SelectionKey.OP_READ or SelectionKey.OP_WRITE)
-        } else {
-            key.interestOps(SelectionKey.OP_READ)
-        }
-    }
-
-    fun register(selector: Selector, channel: SocketChannel) {
-        channel.configureBlocking(false)
-        channel.register(selector, SelectionKey.OP_READ)
-    }
-
-    // public static Process start() throws IOException {
-    fun start(port: Int, ip: String, ipPort: Int): Process {
-        return ProcessBuilder(
-            "java",
-            "-cp",
-            "out/production/SelectServer",
-            "selectserver.EchoServer",
-            port.toString(),
-            ip,
-            ipPort.toString()
-        ).start() ?: throw RuntimeException("Could not start process")
-
-//        val process = Runtime.getRuntime().exec("nc -l $port")
-//        val inputStream = process.inputStream
-//        val outputStream = process.outputStream
-//
-//        val selector = Selector.open()
-//        val serverSocketChannel = ServerSocketChannel.open()
-//        serverSocketChannel.configureBlocking(false)
-//        val serverSocket = serverSocketChannel.socket()
-//        serverSocket.bind(InetSocketAddress(ip, ipPort))
-//        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT)
-//
-//        val buffer = ByteBuffer.allocate(1024)
-//        while (true) {
-//            selector.select()
-//            val keys = selector.selectedKeys()
-//            val iterator = keys.iterator()
-//            while (iterator.hasNext()) {
-//                val key = iterator.next()
-//                iterator.remove()
-//                if (key.isAcceptable()) {
-//                    val server = key.channel() as ServerSocketChannel
-//                    val client = server.accept()
-//                    register(selector, client)
-//                } else if (key.isReadable()) {
-//                    val channel = key.channel() as SocketChannel
-//                    val read = channel.read(buffer)
-//                    if (read == -1) {
-//                        channel.close()
-//                    } else {
-//                        answerWithEcho(buffer, key)
-//                    }
-//                } else if (key.isWritable()) {
-//                    answerWithEcho(buffer, key)
-//                }
-//            }
-//        }
-//        return ProcessBuilder("nc", "-l", "9090").start() ?: throw IOException("Process is null")
-    }
-
-    fun main(args : Array<String>) {
-        val port = if (args.isNotEmpty()) args[0].toInt() else 9090
-        val ip = if (args.size > 1) args[1] else "localhost"
-        val ipPort = if (args.size > 2) args[2].toInt() else 80
-
-        val selector = Selector.open()
-        val serverSocket = ServerSocket(9090)
-
-        if (serverSocket.channel == null) {
-            throw RuntimeException("Could not open server socket channel")
-        }
-
-        val serverChannel = serverSocket.channel
-        serverChannel.configureBlocking(false)
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT)
-
-        val buffer = ByteBuffer.allocate(1024)
-        while (true) {
-            selector.select()
-            val keys = selector.selectedKeys()
-            val iterator = keys.iterator()
-            while (iterator.hasNext()) {
-                val key = iterator.next()
-                iterator.remove()
-                if (key.isAcceptable()) {
-                    val server = key.channel() as ServerSocketChannel
-                    val client = server.accept()
-                    register(selector, client)
-                } else if (key.isReadable()) {
-                    val channel = key.channel() as SocketChannel
-                    val read = channel.read(buffer)
-                    if (read == -1) {
-                        channel.close()
+    while (true) {
+        selector.select()
+        val selectedKeys = selector.selectedKeys()
+        val iterator = selectedKeys.iterator()
+        while (iterator.hasNext()) {
+            val key = iterator.next()
+            if (key.isAcceptable) {
+                val conn = serverSocketChanel.accept()
+                conn.configureBlocking(false)
+                conn.register(selector, SelectionKey.OP_READ)
+                println("Go new connection from ${conn.remoteAddress}")
+            }
+            if (key.isReadable) {
+                val buffer = ByteBuffer.allocate(1024)
+                val client = key.channel() as SocketChannel
+                try{
+                    val n = client.read(buffer)
+                    if (n < 0) {
+                        client.close()
                     } else {
-                        answerWithEcho(buffer, key)
+                        buffer.flip()
+                        val data = ByteArray(buffer.limit())
+                        buffer.get(data)
+                        println(String(data))
+                        // write the response to the client
+                        client.write(ByteBuffer.wrap("HTTP/1.1 200 OK\r\n\r\n".toByteArray()))
+                        client.write(ByteBuffer.wrap("Hello World".toByteArray()))
+                        client.close()
                     }
-                } else if (key.isWritable()) {
-                    answerWithEcho(buffer, key)
+                } catch (ignored:Exception){
+                    client.write(ByteBuffer.wrap("HTTP/1.1 500 Internal Server Error\r\n\r\n".toByteArray()))
+                    client.close()
                 }
             }
+            iterator.remove()
         }
+        println("iteration")
     }
 }
